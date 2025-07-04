a43acaefbcc04e5ac15f3170cbff10edb5b0cf25
ramjan-gh <ramzanhridoy@gmail.com>
2025-07-05 01:09:08 +0600

    balance upgraded

--- js/home.js
+++ js/home.js
@@ -1,1 +1,29 @@
-console.log('home.js added');
+// console.log('home.js added');
+
+// add money
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    // console.log('add money btn clicked');
+const addMoneyInput = document.getElementById("add-money-input");  
+console.log(addMoneyInput.value);
+const pinNumber = document.getElementById('pin');
+console.log(pinNumber.value);  
+if(pinNumber.value === '227'){
+    console.log('Adding TK',addMoneyInput.value,'to your account...');
+    const currentBalance = document.getElementById('current-balance');
+    // console.log(currentBalance.innerText);
+    const addMoneyInputNumber = parseFloat(addMoneyInput.value);
+    console.log(addMoneyInputNumber);
+    const currentBalanceNumber = parseFloat(currentBalance.innerText);
+    console.log(currentBalanceNumber);
+    const newBalance = currentBalanceNumber + addMoneyInputNumber;
+    console.log(newBalance);
+
+    document.getElementById('current-balance').innerText = newBalance;
+}
+else{
+    console.log('Wrong pin')
+}
+});
